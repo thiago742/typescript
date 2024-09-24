2e3be5149b00d16b603b352d9060910b2ff5754e
Thiago F <fthiago742@gmail.com>
2024-09-24 11:33:41 -0300

    exercicio typescript

---
+++ src/exercicio.js
@@ -0,0 +1,5 @@
+"use strict";
+const multiplicar = (a, b) => a * b;
+const darOlá = (nome) => 'Olá ' + nome;
+console.log(multiplicar(30, 10));
+console.log(darOlá('Lucas'));
---
+++ src/exercicio.ts
@@ -0,0 +1,6 @@
+const multiplicar = (a: number, b:number): number => a * b;
+
+const darOlá = (nome: string): string => 'Olá ' + nome;
+
+console.log(multiplicar(30,10))
+console.log(darOlá('Lucas'))
